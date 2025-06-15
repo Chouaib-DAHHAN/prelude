1a4aaa4e37e9648532610d50a03596f08fa6ba5b
Chouaib-DAHHAN <chouaib10md@gmail.com>
2025-06-15 15:35:33 +0100

    commit: first commit

---
+++ src/main/java/glcc/com/prelude/PreludeApplication.java
@@ -0,0 +1,13 @@
+package glcc.com.prelude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreludeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreludeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/glcc/com/prelude/PreludeApplicationTests.java
@@ -0,0 +1,13 @@
+package glcc.com.prelude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreludeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
