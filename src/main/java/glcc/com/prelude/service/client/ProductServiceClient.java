975bc5d4842f924e30a0943f046f953b7116310a
Chouaib-DAHHAN <chouaib10md@gmail.com>
2025-06-26 14:54:28 +0100

    Refactor imports in ProductServiceClient to improve code organization

--- src/main/java/glcc/com/prelude/service/client/ProductServiceClient.java
+++ src/main/java/glcc/com/prelude/service/client/ProductServiceClient.java
@@ -1,12 +1,14 @@
 package glcc.com.prelude.service.client;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Optional;
-import java.util.UUID;
 
+@service
 @FeignClient(name = "product-service", url = "${services.repertoire.url}")
 public interface ProductServiceClient {
 
